34107a1ffba68bbf95c86d118a3ccebcd7fa5988
Noah <noahjames1035@gmail.com>
2025-07-08 10:59:27 -0400

    Navbar started

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+function Navbar() {
+  return (
+    <>
+    <div id="navbar">
+        <div className="w-full max-w-[1920px] h-[8vh] bg-black">
+            <img className="repeat: none"
+            src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/EskFRy5UYJsbY56NXuGe/media/66f9f66c5e30c2e20b29862d.png" alt="" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "../../components/Navbar"
 import "./globals.css";
+import { Poppins } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata = {
@@ -19,9 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={poppins.className}>
+        <Navbar />
         {children}
       </body>
     </html>
